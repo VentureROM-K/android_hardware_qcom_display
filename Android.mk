464725c835343d3a312ea8c405cac41d9a7dca3a
RobertDrone <robertdrone@ovi.com>
2014-09-25 11:17:16 -0600

    potential build error squash
    Adding mkfiles directly to top level make so build points to correct files.

--- Android.mk
+++ Android.mk
@@ -1,6 +1,16 @@
 ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8974 based platforms
     include $(call all-named-subdir-makefiles,msm8974)
+	include hardware/qcom/display/libgenlock/Android.mk
+	include hardware/qcom/display/libcopybit/Android.mk
+	include hardware/qcom/display/libexternal/Android.mk
+	include hardware/qcom/display/libgralloc/Android.mk
+	include hardware/qcom/display/libhwcomposer/Android.mk
+	include hardware/qcom/display/liblight/Android.mk
+	include hardware/qcom/display/liboverlay/Android.mk
+	include hardware/qcom/display/libqdutils/Android.mk
+	include hardware/qcom/display/libqservice/Android.mk
+	include hardware/qcom/display/libtilerenderer/Android.mk
 else
 ifneq ($(filter msm8226,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8226 based platforms
